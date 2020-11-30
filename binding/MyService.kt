5e7fb62db204122229a11c98e8540ad0bd074675
eggling <youcantrememberthis@gmail.com>
2020-11-30 12:41:54 +0100

    comentarios

--- binding/MyService.kt
+++ binding/MyService.kt
@@ -7,16 +7,17 @@ import android.os.IBinder
 import android.widget.Toast
 
 class MyService : Service() {
+    //objeto binder, se declara una clase interna abajo que herede de Binder()
     private val binder = LocalBinder()
-
+    //al hacer bind se devuelve el objeto binder
     override fun onBind(intent: Intent): IBinder {
         Toast.makeText(applicationContext, "Bind Realizado", Toast.LENGTH_SHORT).show()
         return binder
     }
     inner class LocalBinder : Binder() {
-        fun getService(): MyService = this@MyService
+        fun getService(): MyService = this@MyService 
     }
-
+    //función interna del servicio
     fun HolaMundo(){
         Toast.makeText(applicationContext, "Hola mundo", Toast.LENGTH_SHORT).show()
     }
